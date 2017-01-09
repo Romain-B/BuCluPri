80010c58ac7e0d0406fb2c2e638a3cb446cf81d2
rbulteau <romain.bulteau@insa-lyon.fr>
2017-01-09 14:21:26 +0100

    Added print in testing

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "Abr.h"
+#include <iostream>
 
 int main(int argc, char const *argv[])
 {
@@ -6,6 +7,9 @@ int main(int argc, char const *argv[])
   Abr mynoeud(12);
 
   printf("\nICI : %d\n", mynoeud.cle() );
+
+  std::cout<<mynoeud.noeud_g()<<std::endl;
+
 
   return 0;
 }

8a8539ba6b1d3a02f8de2865c065afe376633234
rbulteau <romain.bulteau@insa-lyon.fr>
2017-01-09 17:32:03 +0100

    Organized testing in main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,15 @@ int main(int argc, char const *argv[])
   
   Abr mynoeud(4);
 
+  //Assign test
   mynoeud.assign(5);
   mynoeud.assign(2);
 
-  
-  printf("\nICI AuSSI\n %d\n", mynoeud.noeud_g().cle());
-
+  //Cle test
+  printf("\nCle de noeud.noeud_g\n %d\n", mynoeud.noeud_g().cle());
   mynoeud.noeud_g().assign(3);
-  // g.gauche(1);
 
+  //Search Test
   Abr* se = mynoeud.search(2) ;
   if (se == nullptr)
   {
@@ -26,6 +26,8 @@ int main(int argc, char const *argv[])
     printf("\nICI : %d\n", (*se).cle()); 
   }
 
+
+  
 
   return 0;
 }

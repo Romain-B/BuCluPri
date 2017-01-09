ebb30aeb22a006338f612be3010be860c9000f6a
rbulteau <romain.bulteau@insa-lyon.fr>
2017-01-09 16:48:41 +0100

    Added testing for search and temp setters

--- main.cpp
+++ main.cpp
@@ -4,11 +4,27 @@
 int main(int argc, char const *argv[])
 {
   
-  Abr mynoeud(12);
+  Abr mynoeud(4);
 
-  printf("\nICI : %d\n", mynoeud.cle() );
+  mynoeud.droit(5);
+  mynoeud.gauche(2);
 
-  std::cout<<mynoeud.noeud_g()<<std::endl;
+  
+  printf("\nICI AuSSI\n %d\n", mynoeud.noeud_g().cle());
+
+  mynoeud.noeud_g().droit(3);
+  // g.gauche(1);
+
+  Abr* se = mynoeud.search(15) ;
+  if (se == nullptr)
+  {
+    printf("\nValeur existe pas\n");
+  }
+
+  else
+  {
+    printf("\nICI : %d\n", (*se).cle()); //mynoeud.noeud_g().noeud_d().cle() );
+  }
 
 
   return 0;
